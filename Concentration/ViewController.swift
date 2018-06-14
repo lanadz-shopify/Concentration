51513c2029f7d16a91dfdacc9f8fe9d28a197767
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-06-14 10:23:23 +0800

    Lecture 1 - create Concentration app

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount: Int = 0 {
+    didSet {
+        flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var symbols: Array<String> = ["🦊","🦋","🦊","🐤","🦋","🐸","🌴","🌴","😄","🐸","😄","🐤"]
+    @IBOutlet var cards: [UIButton]!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardIndex = cards.index(of: sender)!
+        flipCard(withEmoji: symbols[cardIndex], on: sender)
     }
 
-
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.6439058386, blue: 0.3423922962, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
-
